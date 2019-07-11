0a931bff231aab536732c923ed2f5982a2506361
alanchenchen <739709491@qq.com>
2019-07-11 09:02:08 +0800

    update test script

--- test/module.test.js
+++ test/module.test.js
@@ -4,8 +4,8 @@ const assert = require('assert')
  * @description mocha的测试用例，此处用的node自带的断言模块assert
  */
 describe('normal module', function() {
-    it('module version should be 0.1.0', function() {
+    it('module version should be 0.1.2', function() {
         const v = require('../package.json').version
-        assert.equal(v, '0.1.0')
+        assert.equal(v, '0.1.2')
     })
 })
